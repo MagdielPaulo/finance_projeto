0cc9e0b8dc60c91041b621ec1cb9b85544cbeda4
MagdielPaulo <magdielpaulopy@gmail.com>
2023-07-12 17:05:45 -0300

    alteracoes na exclusão de contas

--- extrato/models.py
+++ extrato/models.py
@@ -8,11 +8,21 @@ class Valores(models.Model):
     )
     
     valor = models.FloatField()
-    categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)  # Use on_delete=models.CASCADE
     descricao = models.TextField()
     data = models.DateField()
-    conta = models.ForeignKey(Conta, on_delete=models.DO_NOTHING)
+    conta = models.ForeignKey(Conta, on_delete=models.CASCADE)  # Use on_delete=models.CASCADE
     tipo = models.CharField(max_length=1, choices=choice_tipo)
-
+    
     def __str__(self):
         return self.descricao
+
+class Conta(models.Model):
+    # Definição dos campos da conta
+
+    def delete(self, *args, **kwargs):
+        # Excluir registros relacionados na tabela Valores
+        Valores.objects.filter(conta=self).delete()
+
+        # Excluir a conta
+        super().delete(*args, **kwargs)
